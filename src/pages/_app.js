bf72e664b86bb4dd0e60b68345f7cc6ea0baaf87
Elvin-Mworia <elvinmworia@gmail.com>
2024-02-11 14:03:33 +0300

    global store setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,17 @@
+import {Provider} from "react-redux";
+import store from "../store/store";
+import {persistStore} from "redux-persist"
+import {PersistGate} from "redux-persist/integration/react"
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const persistor=persistStore(store);
+  return (<>
+    <Provider store={store}>
+    <PersistGate persistor={persistor}>
+    <Component {...pageProps} />
+    </PersistGate>
+  
+  </Provider>
+
+    </>
+  )
 }
