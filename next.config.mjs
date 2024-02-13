5d566d95d3d28b41da26810aa39ebd06aae13ca2
Elvin-Mworia <elvinmworia@gmail.com>
2024-02-13 23:10:01 +0300

    updated config and gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: true,
+  },
+  env: {
+    WALLETPATH: '/wallet.json',
+  }
 };
 
 export default nextConfig;
