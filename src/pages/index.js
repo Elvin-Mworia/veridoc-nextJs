566f7af55f66aff63e1d21bebb7c841677eae342
Elvin-Mworia <elvinmworia@gmail.com>
2024-04-01 15:28:05 +0300

    changed   dashboard for user folder path /user/dashboard from /dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
             router.push( "/admin/dashboard");
           }
          if(res.data.role=="normalUser"){
-            router.push( "/dashboard");
+            router.push( "/user/dashboard");
           }
      
         }
