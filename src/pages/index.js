1dc01e13e031d199d5b02b3c2a40421f3f1be664
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-13 04:04:25 +0300

    added delayed animation to elements on page load

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import {updateuserinfo} from "../../store/userSlice/userInfo";
 import {updateLoginState} from "../../store/userSlice/loginStatus"
 import { updateStaffStation } from "../../store/userSlice/staffStation";
 import axios from "axios"
+import {motion } from "framer-motion";
 
 export default function Home() {
   const router = useRouter()
@@ -74,9 +75,30 @@ console.log(err);
     console.log(err)
   }
   };
+  const fadeInAnimationDiv = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0, transition: { delay: .8, duration: 0.5, ease: "easeOut" } },
+  };
+  const fadeInAnimationLoginButton = {
+    initial: { x: 100, opacity: 0 }, 
+    animate: { x: 0, opacity: 1, transition: { delay: .5,duration: 0.5, ease: "easeOut" } },
+  };
+  const fadeInAnimationSignupButton = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0, transition: { delay: 1.2, duration: 0.5, ease: "easeInOut" } },
+  };
+  const fadeInAnimationLogo = {
+    initial: { x: -100, opacity: 0 }, 
+    animate: { x: 0, opacity: 1, transition: { delay: .3,duration: 0.5, ease: "easeOut" } },
+  };
   return (
     <div className="h-screen flex flex-col bg-[url(/homepage/courtroom1.jpeg)] bg-origin-padding bg-center bg-cover bg-no-repeat bg-clip-border ">
       <div className="flex h-16 items-center w-full p-6">
+        <motion.div 
+         variants={fadeInAnimationLogo}
+         initial="initial"
+         animate="animate"
+        className="flex flex-row">
         <Image
           className=""
           width={50}
@@ -90,40 +112,52 @@ console.log(err);
             Decentralizing Trust: Fairness for All
           </p>
         </div>
-        <button
+        </motion.div>
+        <motion.button
+          variants={fadeInAnimationLoginButton}
+          initial="initial"
+          animate="animate"
           className="bg-main-blue rounded px-5 py-1 text-white ml-auto"
           onClick={()=>{handleConnect('login')}}
         >
           Login
-        </button>
+        </motion.button>
       </div>
       <div className="grow flex items-center w-full">
         <div className="image-part w-3/6">
           {/* <Image
-            className="opacity-50"
+            className="opacity-20"
             width={500}
             height={500}
-            src="/veridoc_logo.png"
+            src="/homepage/veridoc.jpeg"
             alt="Veridoc Log"
           /> */}
         </div> 
-        <div className="w-3/6 p-4">
-          <h2 className="text-4xl font-bold mb-4 text-white outline-green-950">
+        <motion.div 
+          variants={fadeInAnimationDiv}
+          initial="initial"
+          animate="animate"
+          className="w-3/6 p-4">
+          <h2 className="text-4xl font-black mb-4 text-white">
             Empowering the Judiciary with Blockchain
           </h2>
-          <p className="mb-6 text-white">
+          <p className="mb-6 text-cyan-400 font-semibold ">
             Veridoc introduces a revolutionary e-filing system designed to
             transform judicial document management. Utilizing blockchain
             technology, we offer unmatched security, transparency, and
             efficiency, setting a new standard for legal processes worldwide.
           </p>
           <div>
-            <button className="bg-main-blue rounded px-6 py-2 text-white mx-2" 
+            <motion.button
+              variants={fadeInAnimationSignupButton}
+              initial="initial"
+              animate="animate"
+             className="bg-main-blue rounded px-6 py-2 text-white mx-2 my-5" 
                 onClick={()=>{handleConnect('signup')}}>
               Signup
-            </button>
+            </motion.button>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
