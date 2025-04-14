ccb1ed5654cb48a96222268d57e8516c5718c0b0
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-14 21:14:25 +0300

    refatored to  use the map function and made the input element animate

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,69 +1,72 @@
-import { Box ,VStack,Field,Input,Center} from "@chakra-ui/react"
-import { useSelector,useDispatch} from "react-redux";
-import {userRegInfo} from "../../store/userSlice/userRegistration";
+import { Box, VStack, Field, Input, Center } from "@chakra-ui/react";
+import { useSelector, useDispatch } from "react-redux";
+import { userRegInfo } from "../../store/userSlice/userRegistration";
 import {
-    PasswordInput,
-    PasswordStrengthMeter,
-  } from "@/components/ui/password-input"
+  PasswordInput,
+  PasswordStrengthMeter,
+} from "@/components/ui/password-input";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
-export default function SignupForm({tabsValue}){
+const inputVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { duration: 0.3 } },
+};
 
-    return(
-        <Box width="80%" height="75%" className="m-.5">
-            <Center>
-            <form className="flex flex-col space-y-4">
-                <Center>
-                    <VStack>
-        <Field.Root required>
-        <Field.Label>
-          First Name <Field.RequiredIndicator />
-        </Field.Label>
-        <Input placeholder="Joe" variant="subtle"  className="p-2" size="xs"/>
-      </Field.Root>
+export default function SignupForm({ tabsValue }) {
+  const inputFields = [
+    { label: "First Name", placeholder: "Joe", type: "text" },
+    { label: "Last Name", placeholder: "Doe", type: "text" },
+    { label: "Email", placeholder: "joedoe@gmail.com", type: "email" },
+    { label: "Phone Number", placeholder: "070000000", type: "tel" },
+    { label: "Password", placeholder: "password", type: "password", isPassword: true },
+    { label: "Confirm Password", placeholder: "password", type: "password", isConfirmPassword: true },
+  ];
 
-      <Field.Root required>
-        
-        <Field.Label>
-          Last Name <Field.RequiredIndicator />
-        </Field.Label>
-        <Input placeholder="Doe" variant="subtle" className="p-2" size="xs"/>
-      </Field.Root>
-
-      <Field.Root required>
-        
-        <Field.Label>
-          Email <Field.RequiredIndicator />
-        </Field.Label>
-        <Input placeholder="joedoe@gmail.com" variant="subtle" className="p-2" size="xs" />
-      </Field.Root>
-      <Field.Root required>
-        
-        <Field.Label>
-          Phone Number <Field.RequiredIndicator />
-        </Field.Label>
-        <Input placeholder="070000000" variant="subtle" className="p-2" size="xs" />
-      </Field.Root>
-      <Field.Root required>
-        <Field.Label>
-          Password <Field.RequiredIndicator />
-        </Field.Label>
-       < PasswordInput  variant="subtle" placeholder="password" className="p-2"  size="xs"/>
-       <PasswordStrengthMeter value={2}/>
-      </Field.Root>
-      
-      <Field.Root required>
-         <Field.Label>
-         Confirm Password <Field.RequiredIndicator />
-        </Field.Label>
-       < PasswordInput  variant="subtle" placeholder="password" className="p-2"  size="xs"/>
-      </Field.Root>
-      
-        </VStack>
-        </Center>
- 
-            </form>
-            </Center>
-        </Box>
-    )
-
+  return (
+    <Box width="80%" height="75%" className="m-.5">
+      <Center>
+        <form className="flex flex-col space-y-4">
+          <Center>
+            <VStack spacing={4}>
+              {inputFields.map((field, index) => (
+                <motion.div
+                  key={index}
+                  variants={inputVariants}
+                  initial="initial"
+                  animate={{
+                    opacity: 1,
+                    transition: { duration: 1, delay: index * 0.2 ,ease:"easeOut"}, // Stagger delay
+                  }}
+                >
+                  <Field.Root required>
+                    <Field.Label>
+                      {field.label} <Field.RequiredIndicator />
+                    </Field.Label>
+                    {field.isPassword || field.isConfirmPassword ? (
+                      <PasswordInput
+                        variant="subtle"
+                        placeholder={field.placeholder}
+                        className="p-2"
+                        size="xs"
+                      />
+                    ) : (
+                      <Input
+                        placeholder={field.placeholder}
+                        variant="subtle"
+                        className="p-2"
+                        size="xs"
+                        type={field.type}
+                      />
+                    )}
+                    {index === 4 && <PasswordStrengthMeter value={2} />}
+                  </Field.Root>
+                </motion.div>
+              ))}
+            </VStack>
+          </Center>
+        </form>
+      </Center>
+    </Box>
+  );
 }
