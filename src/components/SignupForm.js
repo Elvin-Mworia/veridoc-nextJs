1e60178f84b1523fd9641058cbab4d7c61e1efef
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-15 23:26:29 +0300

    reverted to the initial code structure to make handling of state simpler

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -23,61 +23,79 @@ export default function SignupForm({ tabsValue }) {
     { label: "Confirm Password", placeholder: "password", type: "password", isConfirmPassword: true ,stateName:"confirmPassword"},
   ];
 const dispatch=useDispatch();
+const{firstName,lastName,phone,email,password,confirmPassword}=useSelector((state)=>(state.registrationDetail))
+const inputVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { duration: 1 ,ease:"easeOut",delay:0.5 }}
+};
   return (
     <Box width="80%" height="75%" className="m-.5">
       <Center>
         <form className="flex flex-col space-y-4">
           <Center>
             <VStack spacing={4}>
-              {inputFields.map((field, index) => (
-                <motion.div
-                  key={index}
-                  variants={inputVariants}
-                  initial="initial"
-                  animate={{
-                    opacity: 1,
-                    transition: { duration: 1, delay: index * 0.2 ,ease:"easeOut"}, // Stagger delay
-                  }}
-                >
-                  <Field.Root required>
-                    <Field.Label>
-                      {field.label} <Field.RequiredIndicator />
-                    </Field.Label>
-                    {field.isPassword || field.isConfirmPassword ? (
-                      <PasswordInput
-                        variant="subtle"
-                        placeholder={field.placeholder}
-                        className="p-2"
-                        size="xs"
-                        onChange={(e) => {
-                          dispatch(
-                            userRegInfo(
-                              {[field.stateName]: e.target.value}
-                            )
-                          );
-                        }}
-                      />
-                    ) : (
-                      <Input
-                        placeholder={field.placeholder}
-                        variant="subtle"
-                        className="p-2"
-                        size="xs"
-                        type={field.type}
-                        stateName={field.stateName}
-                        onChange={(e) => {
-                          dispatch(
-                            userRegInfo({
-                              [field.stateName]: e.target.value
-                            })
-                          );
-                        }}
-                      />
-                    )}
-                    {index === 4 && <PasswordStrengthMeter value={2} />}
-                  </Field.Root>
-                </motion.div>
-              ))}
+            <Field.Root required>
+        <Field.Label>
+          First Name <Field.RequiredIndicator />
+        </Field.Label>
+        <motion.Input   
+         initial="initial"
+         animate="animate"
+         variants={inputVariants}
+          placeholder="Joe" variant="subtle"  className="p-2 text-black" size="xs" onChange={(e)=>{dispatch(userRegInfo({firstName:e.target.value}))}} value={firstName}/>
+      </Field.Root>
+
+      <Field.Root required>
+        
+        <Field.Label>
+          Last Name <Field.RequiredIndicator />
+        </Field.Label>
+        <motion.Input  initial="initial"
+         animate="animate"
+         variants={inputVariants} placeholder="Doe" variant="subtle" className="p-2 text-black" size="xs" onChange={(e)=>{dispatch(userRegInfo({lastName:e.target.value}))}} value={lastName}/>
+      </Field.Root>
+
+      <Field.Root required>
+        
+        <Field.Label>
+          Email <Field.RequiredIndicator />
+        </Field.Label>
+        <motion.Input initial="initial"
+         animate="animate"
+         variants={inputVariants} placeholder="joedoe@gmail.com" variant="subtle" className="p-2 text-black" size="xs" onChange={(e)=>{dispatch(userRegInfo({email:e.target.value}))}} value={email} />
+      </Field.Root>
+      <Field.Root required>
+        
+        <Field.Label>
+          Phone Number <Field.RequiredIndicator />
+        </Field.Label>
+        <motion.Input initial="initial"
+         animate="animate"
+         variants={inputVariants} placeholder="070000000" variant="subtle" className="p-2 text-black" size="xs" onChange={(e)=>{dispatch(userRegInfo({phone:e.target.value}))}} value={phone} />
+      </Field.Root>
+      <motion.div 
+      initial="initial"
+      animate="animate"
+      variants={inputVariants}>
+      <Field.Root required>
+        <Field.Label>
+          Password <Field.RequiredIndicator />
+        </Field.Label>
+       <PasswordInput  variant="subtle" placeholder="password" className="p-2 text-black"  size="xs" onChange={(e)=>{dispatch(userRegInfo({password:e.target.value}))}} value={password}/>
+       <PasswordStrengthMeter  value={2}/>
+      </Field.Root>
+      </motion.div>
+      <motion.div 
+      initial="initial"
+      animate="animate"
+      variants={inputVariants} size="xs">
+      <Field.Root required>
+         <Field.Label>
+         Confirm Password <Field.RequiredIndicator />
+        </Field.Label>
+       < PasswordInput  variant="subtle" placeholder="password" className="p-2 text-black"  size="xs" onChange={(e)=>{dispatch(userRegInfo({confirmPassword:e.target.value}))}} value={confirmPassword}/>
+      </Field.Root>
+      </motion.div>
             </VStack>
           </Center>
         </form>
