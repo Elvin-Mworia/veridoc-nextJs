bb19724c887b38f82d0a24ff3cf15aa6f669f880
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-17 23:12:35 +0300

    renamed the user registration reducer

--- store/reducers.js
+++ store/reducers.js
@@ -2,6 +2,6 @@ import UserRegInfoSlice from './userSlice/userRegistration';
 import UserInfoSlice from './userSlice/userInfo';
 import LoginSlice from './userSlice/loginStatus';
 
-export const  {userRegInfo}=UserRegInfoSlice.actions;
+export const  {updateUserRegInfo}=UserRegInfoSlice.actions;
 export const  {updateuserinfo}=UserInfoSlice.actions;
 export const  {updateLoginState}=LoginSlice.actions;
