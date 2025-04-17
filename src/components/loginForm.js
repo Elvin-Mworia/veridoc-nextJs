66d3741c19d9ca97fb12daf6cda8c44fe03d0098
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-17 23:15:40 +0300

    login form component

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,13 +1,11 @@
 
 import { Box, VStack, Field, Input, Center } from "@chakra-ui/react";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector} from "react-redux";
 import {PasswordInput,} from "@/components/ui/password-input";
 import { motion } from "framer-motion";
-import { useState } from "react";
-import axios from "axios";
-import {updateuserinfo} from "../../store/userSlice/userInfo";
-import {updateLoginState} from "../../store/userSlice/loginStatus"
-import { updateStaffStation } from "../../store/userSlice/staffStation";
+import { useState,useEffect } from "react";
+import { updateLoginState } from "../../store/reducers";
+
 
 const inputVariants = {
   initial: { opacity: 0 },
@@ -16,41 +14,14 @@ const inputVariants = {
 
 export default function LoginForm({ tabsValue }) {
 
+const {email,password}=useSelector((state)=>(state.login))
 const dispatch=useDispatch();
 const inputVariants = {
   initial: { opacity: 0 },
   animate: { opacity: 1, transition: { duration: 1 ,ease:"easeOut",delay:0.5 }}
 };
-const [email,setEmail]=useState("");
-const [password,setPassword]=useState("");
-   async function handleLogin(auth) {
-    try{
-       axios.post(`${process.env.BACKENDURL}:${process.env.PORT}/users/v2/login`,{email:email,password:password}).then((res)=>{
-        if(res.status===201){
-          dispatch(updateLoginState({loginStatus:true}))
-          //logic to derive the user walletaddress after successful login
-         dispatch(updateuserinfo({walletAddress:walletAddress,role:res.data.role}))
-            alert(res.data.message);
-            console.log(res.data)
-            if(res.data.role=="staff"){
-             dispatch(updateStaffStation({station:res.data.station}))
-              router.push( "/staff/dashboard");
-            }
-            if(res.data.role=="admin"){
-              router.push( "/admin/dashboard");
-            }
-           if(res.data.role=="normalUser"){
-              router.push( "/user/dashboard");
-            }
-          }        
-        }).catch((err)=>{
-          console.log(err);
-        }   
-      )
-  }catch(err){
-    console.log(err);
-   }
-}
+
+ 
 return (
     <Box width="80%" height="75%" className="m-.5">
       <Center>
@@ -64,7 +35,7 @@ return (
         </Field.Label>
         <motion.Input initial="initial"
          animate="animate"
-         variants={inputVariants} placeholder="joedoe@gmail.com" variant="subtle" className="p-2 text-black" size="xs" onChange={(e)=>{setEmail(e.target.value)}} value={email} />
+         variants={inputVariants} placeholder="joedoe@gmail.com" variant="subtle" className="p-2 text-black" size="xs" onChange={(e)=>{dispatch(updateLoginState({email:e.target.value}))}} value={email} />
       </Field.Root>
     
       
@@ -73,20 +44,14 @@ return (
               initial="initial"
               animate="animate"
               variants={inputVariants}
-              maxWidth="264px">
+             >
         <Field.Label>
           Password <Field.RequiredIndicator />
         </Field.Label>
-       <PasswordInput  variant="subtle" placeholder="password" className="p-2 text-black" maxWidth="264px" size="xs" onChange={(e)=>{setPassword(e.target.value)}} value={password}/>
+       <PasswordInput  variant="subtle" placeholder="password" className="p-2 text-black" maxWidth="264px" size="xs"  onChange={(e)=>{dispatch(updateLoginState({password:e.target.value}))}} value={password}/>
        </motion.div>
       </Field.Root>
-        <button
-                    type="submit"
-                    className="bg-main-blue rounded px-5 py-2 text-white mt-4 self-center uppercase text-sm"
-                    onClick={handleLogin}
-                  >
-                    login
-                  </button>
+       
             </VStack>
           </Center>
         </form>
